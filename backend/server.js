9e0e5049bff8c24b9953eeea9dd256548319183d
Hussain Ali Rezaie <hussainalirezaie54@gmail.com>
2026-05-11 07:43:05 -0700

    fix database

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ import projectRoutes from "./Routes/projectRoutes.js";
 import messageRoutes from "./Routes/messageRoutes.js";
 import homeRoutes from "./Routes/homeRoutes.js";
 import skillRoutes from "./Routes/skillRoutes.js";
+
 import aboutRoutes from "./Routes/about.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
